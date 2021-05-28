d91dc7488e95f853d43e9cac9fb6a7679437524d
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-05-28 12:34:40 +0200

    Create tests

---
+++ test_downloader.py
@@ -0,0 +1,30 @@
+import downloader
+
+
+def test_get_places():
+    data = {
+        "countries": [
+            {
+                "name": "nextbike Pardubice",
+                "cities": [
+                    {
+                        "name": "Pardubice",
+                        "places": [
+                            {
+                                "uid": 1,
+                                "name": "Name",
+                                "bikes_available_to_rent": 2
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+    places = downloader.get_places(data)
+
+    assert len(places) == 1
+    assert places[0].uid == 1
+    assert places[0].name == "Name"
+    assert places[0].bikes_available_to_rent == 2
