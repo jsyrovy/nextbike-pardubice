3cf574ef77031ff6f967008ac7c1dca6370f0dab
Jiri Syrovy <jiri.syrovy@gmail.com>
2021-06-02 13:20:08 +0200

    Create extended page

--- publisher.py
+++ publisher.py
@@ -31,7 +31,13 @@ def main() -> None:
 
         charts = get_charts(get_records(conn.cursor()))
         highest_value = max([c.highest_value for c in charts])
-        publish_page(charts, highest_value)
+        title = "Dostupnost za posledních 12 hodin<br><a href='24h.html'>Zobrazit posledních 24 hodin</a>"
+        publish_page(charts, highest_value, title)
+
+        charts_24h = get_charts(get_grouped_records(conn.cursor(), 24))
+        highest_value_24h = max([c.highest_value for c in charts_24h])
+        title_24h = "Dostupnost za posledních 24 hodin<br><a href='index.html'>Zobrazit posledních 12 hodin</a>"
+        publish_page(charts_24h, highest_value_24h, title_24h, "24h.html")
 
 
 def load_places(conn: sqlite3.Connection) -> None:
@@ -83,13 +89,26 @@ def get_records(cursor: sqlite3.Cursor) -> list[Record]:
     return [Record(int(r[0]), r[1], r[2], int(r[3])) for r in cursor.fetchall()]
 
 
-def publish_page(charts: list[Chart], highest_value: int, path="index.html") -> None:
+def get_grouped_records(cursor: sqlite3.Cursor, hours: int) -> list[Record]:
+    cursor.execute(
+        "SELECT p.uid, p.name, STRFTIME('%d %H', dt), ROUND(AVG(bikes_available_to_rent), 0) "
+        "FROM bike_states bs "
+        "JOIN places p ON bs.uid = p.uid "
+        f"WHERE bs.dt >= '{datetime.datetime.now() - datetime.timedelta(hours=hours)}'"
+        "GROUP BY p.uid, p.name, STRFTIME('%d %H', dt)" 
+        "ORDER BY p.name, bs.dt"
+    )
+    return [Record(int(r[0]), r[1], r[2], int(r[3])) for r in cursor.fetchall()]
+
+
+def publish_page(charts: list[Chart], highest_value: int, title: str, path="index.html") -> None:
     page = pathlib.Path(path)
     page.write_text(
         get_template().render(
             charts=charts,
             dt=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
             highest_value=highest_value,
+            title=title,
         ),
         "UTF-8",
     )
